cf0f76e1b8b88c4adb6aa559d03f47728e081d76
Fenna <Fennahornstra@gmail.com>
2021-02-02 11:31:34 +0100

    new Branch

--- RepairShop/ViewModels/HomeIndexViewModel.cs
+++ RepairShop/ViewModels/HomeIndexViewModel.cs
@@ -9,11 +9,12 @@ namespace RepairShop.ViewModels
     public class HomeIndexViewModel
     {
         public IEnumerable<RepairJob> RepairJobs { get; set; }
-        
+        public Dictionary<RepairStatus, int> RepairStatus { get; set; }
         public int RepairJobDoneCount { get; set; }
         public int RepairJobPendingCount { get; set; }
         public int RepairJobWaitingForPartsCount { get; set; }
         public int RepairJobInProgressCount { get; set; }
+
 
 
 
