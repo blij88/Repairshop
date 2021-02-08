9a7456fa85fd23a859ebb308ce9115b471850192
Fenna <Fennahornstra@gmail.com>
2021-02-08 11:02:44 +0100

    errors and initial employee data maybe?

--- RepairShop/ViewModels/HomeIndexViewModel.cs
+++ RepairShop/ViewModels/HomeIndexViewModel.cs
@@ -11,5 +11,6 @@ namespace RepairShop.ViewModels
         public IEnumerable<RepairJob> RepairJobs { get; set; }
         public Dictionary<RepairStatus, int> RepairStatus { get; set; }
         public List<string> HeaderNames { get; set; }
+        public Employee Employee { get; set; }
     }
 }
