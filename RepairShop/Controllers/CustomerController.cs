51d6816a5abb96e8400ca449585551d9b7ed0976
Fenna <Fennahornstra@gmail.com>
2021-02-08 11:55:26 +0100

    created controllers

--- RepairShop/Controllers/CustomerController.cs
+++ RepairShop/Controllers/CustomerController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RepairShop.Data.Models;
+using RepairShop.Data.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +10,31 @@ namespace RepairShop.Controllers
 {
     public class CustomerController : Controller
     {
+        ICustomersData db1;
+
+        public CustomerController(ICustomersData db1)
+        {
+            this.db1 = db1;
+        }
         // GET: Customer
         public ActionResult Overview()
         {
-            return View();
+            var Model = db1.GetAll(); ;
+            return View(Model);
         }
+
 
         public ActionResult Create()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            db1.Add(customer);
+            return RedirectToAction("overview");
         }
     }
 }
--- RepairShop/Controllers/EmployeeController.cs
+++ RepairShop/Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RepairShop.Data.Models;
+using RepairShop.Data.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,29 @@ namespace RepairShop.Controllers
 {
     public class EmployeeController : Controller
     {
+        IEmployeesData db1;
+
+        public EmployeeController(IEmployeesData db1)
+        {
+            this.db1 = db1;
+        }
         // GET: Employee
-        public ActionResult WorkOrders()
+        public ActionResult Overview()
+        {
+            var Model = db1.GetAll() ;
+            return View(Model);
+        }
+
+        public ActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee Employee)
+        {
+            db1.Add(Employee);
+            return RedirectToAction("Overview");
         }
     }
 }
