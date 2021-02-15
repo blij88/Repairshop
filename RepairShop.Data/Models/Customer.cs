4062c018a51dfd81fa9d8865e21b8ce7d6f9717e
Fenna <Fennahornstra@gmail.com>
2021-02-15 12:15:29 +0100

    dicovery

--- RepairShop.Data/Models/Customer.cs
+++ RepairShop.Data/Models/Customer.cs
@@ -11,11 +11,8 @@ namespace RepairShop.Data.Models
     {
         public int Id { get; set; }
 
+
         [Required]
-        public string Name { get; set; }
-        [Phone]
-        public string Phone { get; set; }
-        [EmailAddress]
-        public string Email { get; set; }
+        public string UserId { get; set; }
     }
 }
