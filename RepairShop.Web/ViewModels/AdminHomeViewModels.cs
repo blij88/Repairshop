50da16d9cb08dea18abf9c02688cecc6e31cad0d
Fenna <Fennahornstra@gmail.com>
2021-02-16 10:41:26 +0100

    new Layout

--- RepairShop.Web/ViewModels/AdminHomeViewModels.cs
+++ RepairShop.Web/ViewModels/AdminHomeViewModels.cs
@@ -2,40 +2,28 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace RepairShop.Web.ViewModels
 {
-    public class AdminHomeIndexViewModel
+    public class HomeIndexViewModel
     {
-        public IEnumerable<AdminQueryRepairJob> RepairJobs { get; set; }
+        public IEnumerable<QueryRepairJob> RepairJobs { get; set; }
         public Dictionary<RepairStatus, int> RepairStatus { get; set; }
         public List<string> HeaderNames { get; set; }
         public IEnumerable<Customer> Customer { get; set; }
     }
 
-    public class AdminQueryRepairJob
+    public class QueryRepairJob
     {
         public int Id { get; set; }
+        [DisplayName ("Start date")]
         public DateTime StartDate { get; set; }
+        [DisplayName ("End date")]
         public DateTime EndDate { get; set; }
         public string Customer { get; set; }
         public bool IsLate { get; set; }
         public RepairStatus Status {get; set; }
-    }
-
-    public class AdminHomeDetailsViewModel
-    {
-        public RepairJob RepairJob { get; set; }
-        public Customer Customer { get; set; }
-        public decimal Price { get; set; }
-    }
-
-    public class AdminJobEditViewModel
-    {
-        public RepairJob Job { get; set; }
-        public RepairJobEmployee JobEmployee { get; set; }
     }
 }
