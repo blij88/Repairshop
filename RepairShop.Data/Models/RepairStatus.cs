aff5fed2e1339a2d911497dd5275ecf7a41aacd6
Fenna <Fennahornstra@gmail.com>
2021-02-18 10:31:26 +0100

    working on customer registry

--- RepairShop.Data/Models/RepairStatus.cs
+++ RepairShop.Data/Models/RepairStatus.cs
@@ -1,9 +1,15 @@
-﻿namespace RepairShop.Data.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RepairShop.Data.Models
 {
     public enum RepairStatus
     {
+        [Display(Name = "pending...")]
         Pending,
+        [Display(Name = "In progress")]
         InProgress,
+        [Display(Name = "Waiting for parts")]
         WaitingForComponents,
         Done
     }
